d537d0cc2bc83f35caf01bd97eba13fc2b145402
Uwakmfon1 <udohuwakmfon2@gmail.com>
2025-01-31 13:04:19 +0100

    cached route fn.

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,20 @@
 <?php
 
-use App\Http\Controllers\BasicInfoController;
-use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 
 
 
-Route::get('/basic-info',function(){
-    return response()->json([
+Route::get('/basic-info', function(){
+$data = Cache::remember('basic_info',60,function(){
+    return [
         'email'=>"udohuwakmfon2@gmail.com",
         'current_datetime'=>now()->toIso8601String(),
         'github_url' => "<https://github.com/Uwakmfon1/basicInfo-api-HNG12.0>"
-    ],200);
+    ];
+});
+return response()->json($data);
 });
 
