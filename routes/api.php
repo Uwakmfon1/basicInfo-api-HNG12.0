fc31d541328460ee0c5db802bad4a0f6aed29fae
Uwakmfon1 <udohuwakmfon2@gmail.com>
2025-01-31 10:29:25 +0100

    modified api routing

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/basic-info',[BasicInfoController::class,'index']);
+// Route::get('/basic-info',[BasicInfoController::class,'index']);
+
+Route::get('/basic-info',function(){
+    return response()->json([
+        'email'=>"udohuwakmfon2@gmail.com",
+        'current_datetime'=>date('c'),
+        'github_url' => "<https://github.com/Uwakmfon1/basicInfo-api-HNG12.0>"
+    ],200);
+});
 
