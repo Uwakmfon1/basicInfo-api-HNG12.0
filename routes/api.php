28e63c504eecb6f733f076b60d8517021b1c47af
Uwakmfon1 <udohuwakmfon2@gmail.com>
2025-01-31 10:42:25 +0100

    date()-now() replc.

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/basic-info',function(){
     return response()->json([
         'email'=>"udohuwakmfon2@gmail.com",
-        'current_datetime'=>date('c'),
+        'current_datetime'=>now()->toIso8601String(),
         'github_url' => "<https://github.com/Uwakmfon1/basicInfo-api-HNG12.0>"
     ],200);
 });
