efde086f39276aad008f04d9742c1510c269a074
Uwakmfon1 <udohuwakmfon2@gmail.com>
2025-01-31 14:06:46 +0100

    api route modified

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ Route::get('/basic-info', function(){
     return response()->json([
         'email'=>"udohuwakmfon2@gmail.com",
         'current_datetime'=>now()->utc()->format('Y-m-d\TH:i:s\Z'),
-        'github_url' => "<https://github.com/Uwakmfon1/basicInfo-api-HNG12.0>"
+        'github_url' => "https://github.com/Uwakmfon1/basicInfo-api-HNG12.0"
     ],200);
 });
 
