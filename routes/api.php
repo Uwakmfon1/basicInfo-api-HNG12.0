5593b43bb960bd1fb878f43323ed3379513f974c
Uwakmfon1 <udohuwakmfon2@gmail.com>
2025-01-31 13:13:41 +0100

    removed cached route fn.

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,18 @@
 <?php
 
 
-use Illuminate\Support\Facades\Cache;
+
 use Illuminate\Support\Facades\Route;
 
 
 
 
 Route::get('/basic-info', function(){
-$data = Cache::remember('basic_info',60,function(){
-    return [
+
+    return response()->json([
         'email'=>"udohuwakmfon2@gmail.com",
         'current_datetime'=>now()->toIso8601String(),
         'github_url' => "<https://github.com/Uwakmfon1/basicInfo-api-HNG12.0>"
-    ];
-});
-return response()->json($data);
+    ]);
 });
 
